4aacd2be5db7d8c298548727cee1237b6721d069
Ramahadi Jan <moheanerj@gmail.com>
2021-08-19 08:39:46 +0200

    completed setting widget

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -7,6 +7,11 @@
 
 //create a variable that will keep track of the total bill
 
+
+
+
+
+
 //add an event listener for when the add button is pressed
 
 //in the event listener get the value from the billItemTypeRadio radio buttons
@@ -18,19 +23,27 @@ let callTotalSettings = document.querySelector(".callTotalSettings");
 let smsTotalSettings = document.querySelector(".smsTotalSettings");
 let totalSettings = document.querySelector(".totalSettings");
 let radioaddSettings = document.querySelector(".radioaddSettings");
-
+let updateSettings = document.querySelector(".updateSettings");
+let callCostSetting = document.querySelector(".callCostSetting");
+let smsCostSetting = document.querySelector(".smsCostSetting");
+let warningLevelSetting = document.querySelector(".warningLevelSetting");
+let criticalLevelSetting = document.querySelector(".criticalLevelSetting");
 
 var radioTotalCalls = 0;
 var radioTotalSms = 0;
+var callCost = 2.75;
+var smsCost = 0.75;
+var warningLevel = 30;
+var dangerLevel = 50;
 
 function radioBillTotal() {
     const checkedBtn = document.querySelector(".billItemTypeWithSettings:checked");
     let billItemType = checkedBtn.value;
     if (billItemType == "call") {
-        radioTotalCalls += 2.75;
+        radioTotalCalls += callCost;
     }
     else if (billItemType == "sms") {
-        radioTotalSms += 0.75;
+        radioTotalSms += smsCost;
     }
 
     callTotalSettings.innerHTML = radioTotalCalls.toFixed(2);
@@ -38,15 +51,45 @@ function radioBillTotal() {
     let radioTotal= radioTotalCalls + radioTotalSms;
     let number = radioTotal.toFixed(2);
     
-    if (radioTotal >= 30 && radioTotal < 50) {
+    if (radioTotal >= warningLevel && radioTotal < dangerLevel) {
         final.classList.add("warning");
     }
-    else if (radioTotal >= 50) {
+    else if (radioTotal >= dangerLevel) {
         final.classList.add("danger");
-        number = 50;
+        number = dangerLevel;
     }
     totalSettings.innerHTML = number;
     console.log(radioTotalCalls)
 }
 
 radioaddSettings.addEventListener('click', radioBillTotal);
+
+
+function updateFunc() {
+
+    console.log(costperCall)
+    totalSettings.classList.remove("danger");
+    totalSettings.classList.remove("warning");
+    var costperCall = parseFloat(callCostSetting.value);
+    if (costperCall != 0) {
+        callCost = costperCall;
+        settingCalltotal = 0;
+    }
+    var costperSms = parseFloat(smsCostSetting.value);
+    if (costperSms != 0) {
+        smsCost = costperSms;
+        settingSmstotal = 0;
+    }
+
+    var warningValue = parseFloat(warningLevelSetting.value);
+    if (warningValue != callCost - smsCost) {
+        warningLevel = warningValue;
+    }
+    var dangerValue = parseFloat(criticalLevelSetting.value);
+    if (dangerValue != callCost - smsCost) {
+        dangerLevel = dangerValue;
+    }
+
+}
+
+updateSettings.addEventListener('click', updateFunc)
