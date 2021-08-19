1ec2b28785526c8869ffd85e181a68df38ab6531
Ramahadi Jan <moheanerj@gmail.com>
2021-08-19 07:58:20 +0200

    updated bill type section on setting widget

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -1,22 +1,52 @@
-// get a reference to the sms or call radio buttons
 
-// get refences to all the settings fields
+//text bill
+
+// get a reference to the sms or call radio buttons
 
 //get a reference to the add button
 
-//get a reference to the 'Update settings' button
-
-// create a variables that will keep track of all the settings
-
-// create a variables that will keep track of all three totals.
-
-//add an event listener for when the 'Update settings' button is pressed
+//create a variable that will keep track of the total bill
 
 //add an event listener for when the add button is pressed
 
 //in the event listener get the value from the billItemTypeRadio radio buttons
-// * add the appropriate value to the call / sms total
-// * add the appropriate value to the overall total
+// * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
-// * display the latest total on the screen.
-// * check the value thresholds and display the total value in the right color.
+// * display the latest total on the screen
+let final = document.querySelector(".final");
+let callTotalSettings = document.querySelector(".callTotalSettings");
+let smsTotalSettings = document.querySelector(".smsTotalSettings");
+let totalSettings = document.querySelector(".totalSettings");
+let radioaddSettings = document.querySelector(".radioaddSettings");
+
+
+var radioTotalCalls = 0;
+var radioTotalSms = 0;
+
+function radioBillTotal() {
+    const checkedBtn = document.querySelector(".billItemTypeWithSettings:checked");
+    let billItemType = checkedBtn.value;
+    if (billItemType == "call") {
+        radioTotalCalls += 2.75;
+    }
+    else if (billItemType == "sms") {
+        radioTotalSms += 0.75;
+    }
+
+    callTotalSettings.innerHTML = radioTotalCalls.toFixed(2);
+    smsTotalSettings.innerHTML = radioTotalSms.toFixed(2);
+    let radioTotal= radioTotalCalls + radioTotalSms;
+    let number = radioTotal.toFixed(2);
+    
+    if (radioTotal >= 30 && radioTotal < 50) {
+        final.classList.add("warning");
+    }
+    else if (radioTotal >= 50) {
+        final.classList.add("danger");
+        number = 50;
+    }
+    totalSettings.innerHTML = number;
+    console.log(radioTotalCalls)
+}
+
+radioaddSettings.addEventListener('click', radioBillTotal);
